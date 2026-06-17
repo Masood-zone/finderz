a1c05bab70636097591b27c38b8ac04d6b1510d6
masood <masoodacheampong@gmail.com>
2026-06-17 11:24:47 +0000

    feat: add profile info and logout to super-admin dashboard

--- src/app/(super-admin)/super-admin/dashboard.tsx
+++ src/app/(super-admin)/super-admin/dashboard.tsx
@@ -1,13 +1,45 @@
-import { Text, View } from "react-native";
+import { AppButton } from "@/components/ui/app-button";
+import { signOut } from "@/lib/auth-client";
 import { useSuperAdminDashboard } from "@/services/queries/hooks";
+import { router } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function SuperAdminDashboardScreen() {
   const dashboard = useSuperAdminDashboard();
-
+  const logOut = async () => {
+    await signOut();
+    router.replace("/sign-in");
+  };
   return (
     <View className="flex-1 justify-center bg-background px-6">
-      <Text className="text-2xl font-bold text-text-primary">Super Administrator dashboard</Text>
-      <Text className="mt-2 text-base text-text-secondary">Status: {dashboard.status}</Text>
+      <Text className="text-2xl font-bold text-text-primary">
+        Super Administrator dashboard
+      </Text>
+      <Text className="mt-2 text-base text-text-secondary">
+        Status: {dashboard.status}
+      </Text>
+
+      {/* Profile data */}
+      <View className="mt-4">
+        <Text className="text-lg font-semibold text-text-primary">
+          Profile Information
+        </Text>
+        <Text className="mt-2 text-base text-text-secondary">
+          Name: {dashboard?.data?.user?.name}
+        </Text>
+        <Text className="text-base text-text-secondary">
+          Email: {dashboard?.data?.user?.email}
+        </Text>
+
+        <AppButton
+          title="Log out"
+          variant="danger"
+          onPress={() => {
+            logOut();
+          }}
+          className="mt-4"
+        />
+      </View>
     </View>
   );
 }
