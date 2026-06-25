189625318406ce49918608b549c600ed69e84bc9
masood <masoodacheampong@gmail.com>
2026-06-25 19:55:56 +0000

    fix:sentry integration to Metro config file

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,7 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withSentryConfig } = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 const { withNativewind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
-module.exports = withSentryConfig(withNativewind(config));
+module.exports = withNativewind(config);
